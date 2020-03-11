7c801087c9a0195bd085d4b56d5ac8c3af32a27d
Basko-Vladimir <1989bvg@gmail.com>
2020-03-11 22:58:22 +0300

    Home work №2

--- src/components/MessageTelegram/MessageTelegram.jsx
+++ src/components/MessageTelegram/MessageTelegram.jsx
@@ -5,10 +5,7 @@ const MessageTelegram = (props) => {
     return (
         <div className={styles.messageBlock}>
             <h3 className={styles.senderName}>Басько Владимир</h3>
-            <p className={styles.message}>Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Amet ducimus esse harum magni nesciunt officiis
-                perspiciatis voluptatem. Ex, quod rem.
-            </p>
+            <p className={styles.message}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
             <p className={styles.time}>{props.time}</p>
         </div>
     )
--- src/components/MyName/MyName.jsx
+++ src/components/MyName/MyName.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import styles from "./MyName.module.css";
 
 const MyName = () => {
+    let myQualities = ['Рационалист', 'Студент', 'Анимешник', 'Коммунист'];
     return (
         <div className={styles.myNameBlock}>
             <h1>Басько Владимир Георгиевич</h1>
+            <h2>Мои качества</h2>
+            <ul>
+                {myQualities.map((quality, i) => <li className={(i === 1) ? styles.specialQuality : styles.quality} key={i}>{quality}</li>)}
+            </ul>
         </div>
     )
 };
