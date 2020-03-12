ff75dd6c899159598685dd5e4a0722b8b05b38e8
Basko-Vladimir <1989bvg@gmail.com>
2020-03-12 09:47:50 +0300

    Home work №2 (refactor)

--- src/components/MessageTelegram/MessageTelegram.jsx
+++ src/components/MessageTelegram/MessageTelegram.jsx
@@ -3,10 +3,15 @@ import styles from "./MessageTelegram.module.css";
 
 const MessageTelegram = (props) => {
     return (
-        <div className={styles.messageBlock}>
-            <h3 className={styles.senderName}>Басько Владимир</h3>
-            <p className={styles.message}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-            <p className={styles.time}>{props.time}</p>
+        <div className={styles.messageWrap}>
+            <img className={styles.avatar}
+                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQE8ONSyYrM-yJMooletyTZ-eHJF37_HHkP8SPxNHrGJ04X028k"
+                 alt="ava"/>
+            <div className={styles.messageBlock}>
+                <h3 className={styles.senderName}>Басько Владимир</h3>
+                <p className={styles.message}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                <p className={styles.time}>{props.time}</p>
+            </div>
         </div>
     )
 };
