3f330a1a28485402f45ec29e6d235b815a19c727
Basko-Vladimir <1989bvg@gmail.com>
2020-03-24 14:55:35 +0300

    Updates

---
+++ precache-manifest.bfe4001aee9042ea24f266d39895c7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616cd5368cf0e3784cf6d995df6fccd0",
+    "url": "/Ignat-tasks/index.html"
+  },
+  {
+    "revision": "8daa9690ab3a571f28fc",
+    "url": "/Ignat-tasks/static/css/main.6db7a6e0.chunk.css"
+  },
+  {
+    "revision": "c5aa8d182d60cb3d1202",
+    "url": "/Ignat-tasks/static/js/2.e455e502.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Ignat-tasks/static/js/2.e455e502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8daa9690ab3a571f28fc",
+    "url": "/Ignat-tasks/static/js/main.0149a5b6.chunk.js"
+  },
+  {
+    "revision": "cab93f40fbf47a29803b",
+    "url": "/Ignat-tasks/static/js/runtime-main.6866f146.js"
+  }
+]);
