6b0076ea6ea909dcd0d735b3816336c7ddc1c9f6
Basko-Vladimir <1989bvg@gmail.com>
2020-04-26 10:13:46 +0300

    Updates

--- precache-manifest.f1db55a9d125e33a23d9a40cfa0b8e3c.js
+++ precache-manifest.f1db55a9d125e33a23d9a40cfa0b8e3c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2d37149c5b840dd86cbaca492caead8",
+    "revision": "8f79325ac59df946242ff43028fe9388",
     "url": "/Ignat-tasks/index.html"
   },
   {
-    "revision": "039f0dd5dd3d7fb303e3",
+    "revision": "92e75156d35b218b429b",
     "url": "/Ignat-tasks/static/css/main.91d5dbce.chunk.css"
   },
   {
-    "revision": "016cd3fe429a1f0977ee",
-    "url": "/Ignat-tasks/static/js/2.f2b311c0.chunk.js"
+    "revision": "199db2efbba182aa23f8",
+    "url": "/Ignat-tasks/static/js/2.da92908a.chunk.js"
   },
   {
     "revision": "eb56b19eba63a53416f4fd778023529f",
-    "url": "/Ignat-tasks/static/js/2.f2b311c0.chunk.js.LICENSE.txt"
+    "url": "/Ignat-tasks/static/js/2.da92908a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "039f0dd5dd3d7fb303e3",
-    "url": "/Ignat-tasks/static/js/main.42d5850f.chunk.js"
+    "revision": "92e75156d35b218b429b",
+    "url": "/Ignat-tasks/static/js/main.299ba6ba.chunk.js"
   },
   {
     "revision": "cab93f40fbf47a29803b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ignat-tasks/precache-manifest.c5da97c627327555dd12b511cbb7ee66.js"
+  "/Ignat-tasks/precache-manifest.f1db55a9d125e33a23d9a40cfa0b8e3c.js"
 );
 
 self.addEventListener('message', (event) => {
